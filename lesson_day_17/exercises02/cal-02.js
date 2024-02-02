dcf6c05e441453a5a7156f2975f7a36b9b31ae37
ttulgaa <munhtulga6915@gmailcom>
2024-02-03 00:12:31 +0800

    1234

--- lesson_day_17/exercises02/cal-02.js
+++ lesson_day_17/exercises02/cal-02.js
@@ -1,9 +1,5 @@
 const ButtonElements = document.querySelectorAll(".btn")
-delete ButtonElements[0]
 console.log(ButtonElements)
-const stringNumber = [0, 1, 2, 3, 4, 5]
-//delete stringNumber[2]
-console.log(stringNumber)
 let inputElement = document.getElementById("inputnumber")
 function operation(operator, operand, operation){
     if (operation==="+"){
